e80d4877211e83d2bbe752f68101601dc2bbc77e
AdvaithMahendrakar12 <m.advaith2004@gmail.com>
2026-05-24 23:18:35 +0530

    Removed gRPC verbose folder

--- src/grpc/index.ts
+++
@@ -1,9 +0,0 @@
-// gRPC module barrel export
-export { JobServiceDef, WorkerServiceDef, grpc, PROTO_PATH } from './loader.js';
-export { createServer } from './server.js';
-export {
-  createJobClient,
-  createWorkerClient,
-  createJobClientAsync,
-  createWorkerClientAsync,
-} from './client.js';
